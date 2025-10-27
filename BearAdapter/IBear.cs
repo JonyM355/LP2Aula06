3072a913bf19c37a2890ccabd949caa54ea3de80
JonyM355 <joaoricardoam05@gmail.com>
2025-10-27 16:30:31 +0000

    Copying classes and interfaces

---
+++ BearAdapter/IBear.cs
@@ -0,0 +1,12 @@
+﻿namespace BearAdapter
+{
+    public interface IBear
+    {
+        bool Hibernating { get; }
+
+        void Roar();
+        void LookAt(object objectToLookAt);
+        void GoTowards(object objectToWalkTowards);
+        bool TryEat(object objectToEat);
+    }
+}
